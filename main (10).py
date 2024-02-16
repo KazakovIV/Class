7412108325e4cab62063c79b8cda80d88b073a5e
KazakovIV <158293593+KazakovIV@users.noreply.github.com>
2024-02-16 11:27:43 +0300

    Add files via upload

---
+++ main (10).py
@@ -0,0 +1,20 @@
+first_number = 10
+second_number = 20
+max_value = max(first_number,second_number)
+min_value = min(first_number,second_number)
+print('Максимальное значение:(20)')
+print('Минимальное значение:(10)')
+
+
+
+    
+
+
+
+    
+    
+
+    
+
+
+

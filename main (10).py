0e6d8bad7569a455ee0185bc25f36407df862a59
KazakovIV <158293593+KazakovIV@users.noreply.github.com>
2024-02-20 15:34:35 +0300

    Add files via upload

--- main (10).py
+++ main (10).py
@@ -1,20 +1,9 @@
-first_number = 10
-second_number = 20
-max_value = max(first_number,second_number)
-min_value = min(first_number,second_number)
-print('Максимальное значение:(20)')
-print('Минимальное значение:(10)')
+my_tuple = (True,786,3.14,'text',70.2)
+second_tuple = (123,'text')
 
+print ('my_tuple') 
+print ('my_tuple[0]')
+print ('second_tuple *2')
+print ('my_tuple + second_tuple')
 
-
-    
-
-
-
-    
-    
-
-    
-
-
-
+ 

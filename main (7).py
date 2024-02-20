c9c0ec50fee3118f9cf274dd82b6adf2bd234d90
KazakovIV <158293593+KazakovIV@users.noreply.github.com>
2024-02-20 15:11:44 +0300

    Add files via upload

---
+++ main (7).py
@@ -0,0 +1,24 @@
+# 1)
+a = 7.3
+b = 3
+c = 0
+stroka = 'abc'
+c = a * b
+print(c, type(c))
+c = int ( c )
+print(c, type(c))
+nubm = c
+print (c)
+stroka = stroka * 2 + '!'
+print (stroka)
+
+
+# 2)
+name = str(input('Введите имя:'))
+age = float (input('Введите Ваш вораст:'))
+print('Рад приветствовать тебя', name + '!', 'Тебе уже', int(age), 'лет.')
+
+# 3)
+a = int(input())
+b = int(input())
+print(a+b, a-b, a*b, a/b)
